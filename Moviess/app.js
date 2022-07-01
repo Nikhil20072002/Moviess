f78c75d17a87bcc66a36f0a3c35c12354f100106
Nikhil20072002 <81954184+Nikhil20072002@users.noreply.github.com>
2022-07-01 22:31:09 +0530

    Add files via upload

---
+++ Moviess/app.js
@@ -0,0 +1,36 @@
+var c = 0;
+const search = () => {
+    const searchbox = document.getElementById("search-bar").value.toUpperCase();
+    const movies = document.getElementById("movies");
+    const movie = document.querySelectorAll(".movie");
+    const pname = movies.getElementsByTagName("h3");
+    for (var i = 0; i < pname.length; i++) {
+        let match = movie[i].getElementsByTagName("h3")[0];
+        if (match) {
+            let x = match.textContent || match.innerHTML
+            if (x.toUpperCase().indexOf(searchbox) > -1) {
+                movie[i].style.display = "";
+            } else {
+                movie[i].style.display = "none";
+            }
+        }
+    }
+}
+// const getname = (n) => {
+//     const movie = document.querySelectorAll(".movie");
+//     for (var i = 0; i <= movie.length; i++) {
+//         let match = movie[i].querySelector(".movie_name").textContent
+//         if (match == n) {
+//             localStorage.setItem("movie_name", match);
+//             break;
+//         }
+//     }
+// }
+const getDetails=(name,year,type,director,hero,heroine,villain,imgtype)=>{
+            localStorage.setItem("movie_name", name);
+            localStorage.setItem("year",year);
+            localStorage.setItem("type",type);
+            localStorage.setItem("director",director);
+            localStorage.setItem("cast",hero+","+heroine+","+villain);
+            localStorage.setItem("imgtype",imgtype);
+}
